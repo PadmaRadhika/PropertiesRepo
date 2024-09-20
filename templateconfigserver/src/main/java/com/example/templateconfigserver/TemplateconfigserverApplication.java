72005b71a5504f73b84750367fd188d45203bacd
PadmaRadhika <radhika.piratla@gmail.com>
2024-09-19 20:46:30 -0700

    added properties file

---
+++ templateconfigserver/src/main/java/com/example/templateconfigserver/TemplateconfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.templateconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateconfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateconfigserverApplication.class, args);
+	}
+
+}
